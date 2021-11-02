d0948a7f2f17c722827b135bf5eba89d7cf4db2e
Timur <timos.gooogle@gmail.com>
2021-11-02 10:58:35 +0500

    Bug fix in HelloServlet.java

--- src/main/java/ru/ibs/www/servletsimple/HelloServlet.java
+++ src/main/java/ru/ibs/www/servletsimple/HelloServlet.java
@@ -1,22 +1,32 @@
 package ru.ibs.www.servletsimple;
 
 import com.google.gson.Gson;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import ru.ibs.www.manager.Manager;
 
 import java.io.*;
-import javax.servlet.http.*;
-import javax.servlet.annotation.*;
 
-@WebServlet(name = "helloServlet", value = "/hello-servlet")
+
+@WebServlet("/hello-servlet")
 public class HelloServlet extends HttpServlet {
     private final Gson gson = new Gson();
 
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html");
         Manager managers = new Manager(
                 22,
                 "Vasya",
                 50000
         );
+        String managersJson = this.gson.toJson(managers);
+        PrintWriter out = response.getWriter();
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+        out.print(managersJson);
+        out.flush();
     }
 }
